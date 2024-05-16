e5055208d4296c9cc1b1ac4230eeeb4009fd9851
BenjaminArtunduaga <benjaartunduaga00@gmail.com>
2024-05-16 18:48:36 -0300

    primera-entrega

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,22 @@
 import Carrito from "./Carrito"
+import ItemListContainer from "./ItemListContainer"
 
 
 
 const NavBar = () => {
     return (
 <header className="header">
-    <h1> benyamanco shop</h1>
+    <h1> Ivess</h1>
     <nav className="nav">
         <ul className="nav-menu">
             <li><a className="nav-link" href="#">inicio</a></li>
-            <li><a className="nav-link" href="#">camperas</a></li>
-            <li><a className="nav-link" href="#">camisetas</a></li>
-            <li> <a className="nav-link" href="#">pantalones</a></li>
+            <li><a className="nav-link" href="#">Productos</a></li>
+            <li><a className="nav-link" href="#">Servicios</a></li>
+            <li> <a className="nav-link" href="#">Contacto</a></li>
         </ul>
     </nav>
     <Carrito />
+
 </header>
     )
 }
